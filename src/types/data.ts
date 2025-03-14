ed94cae92488ceb77f1598e11b364228a0b7645c
lee kiri <mango.man.e.ya@gmail.com>
2025-03-14 12:33:12 +0900

    4단계 설명 적용

--- src/types/data.ts
+++ src/types/data.ts
@@ -7,3 +7,13 @@ type News = {
   createdAt: number;
 };
 export type NewsData = News[];
+
+export type Blogs = {
+    id: string,
+      title: string
+      content: string
+      author: string
+      category:string
+      createdAt: number
+      tags: string[]
+}
